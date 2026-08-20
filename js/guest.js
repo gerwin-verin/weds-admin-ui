2d1a8f19bc9385e0b588c6177f84d5cd7583e242
gerwin-verin <gerwin-verin@gmail.com>
2026-08-20 12:53:52 +0700

    commit

--- js/guest.js
+++ js/guest.js
@@ -9,7 +9,7 @@ $(document).ready(function async() {
 function redrawList() {
   var table = $("#table2").DataTable();
 
-  table.row = [];
+  table.clear().draw();
   fetchLoadGuest().then((listGuest) => {
     
     for (var i = 0; i < listGuest.length; i++) {
